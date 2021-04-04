59614b7a89d043679f3b2c2c507f65f5e113d748
ikoide <ian.koide@protonmail.com>
2021-04-03 21:07:50 -0400

    moving code into functions, some comments and overall better JS code

--- bot.js
+++ bot.js
@@ -2,122 +2,180 @@ const mineflayer = require("mineflayer")
 const pathfinder = require("mineflayer-pathfinder").pathfinder
 const Movements = require("mineflayer-pathfinder").Movements
 const { GoalNear } = require("mineflayer-pathfinder").goals
-const Vec3 = require("vec3").Vec3;
+const v = require("vec3")
 
 const bot = mineflayer.createBot({
   host: "192.168.1.178",
-  username: "FaytheBot",
+  username: "FaytheWorker001",
+  port: 55897,
   // password: "",
-  port: 52826,
   // version: false,
   // auth: "mojang"
 })
 
 bot.loadPlugin(pathfinder)
 
-// Input that would be received from the web server
-const TRADER_USERNAME = "_L4N"
-const LOCATION = 0
-
-function isTrader(username) {
-  if (username == TRADER_USERNAME) {
-    bot.chat("TRADE EXECUTED.")
-
-    return true
-  } else {
-    return false
-  }
-}
-
-function goHome() {
-  const mcData = require("minecraft-data")(bot.version)
-  const defaultMove = new Movements(bot, mcData)
-
-  // First execute /warp home
-  const targets = bot.findBlocks({
-    point: bot.entity.position,
-    matching: 152,
-    maxDistance: 128,
-    count: 2
-  })
-  if (!targets) { return }
-
-  const block0 = bot.blockAt(targets[0])
-  const block1 = bot.blockAt(targets[1])
-
-  bot.pathfinder.setMovements(defaultMove)
-  bot.pathfinder.setGoal(new GoalNear(block1.position.x, block1.position.y+1, block1.position.z, 0))  
-}
-
-function getSigns() {
-  return bot.findBlocks({
-    point: bot.entity.position,
-    matching: 68,
-    maxDistance: 128,
-    count: 256
-  })
-}
-
-function storeItems() {
-  const mcData = require("minecraft-data")(bot.version)
-  const defaultMove = new Movements(bot, mcData)
-
-  // Find all signs and check if each inventory item is in any of the signs
-  const signs = getSigns()
-  for (var i = 0; i < signs.length; i++) {
-    for (var j = 0; j < bot.inventory.slots.length; j++) {
-      if (bot.inventory.slots[j]) {
-        if (bot.blockAt(signs[i]).signText.includes(bot.inventory.slots[j].displayName)) {
-          // If there is already a sign with an item that is in Faythe's inventory
-          bot.pathfinder.setMovements(defaultMove)
-          bot.pathfinder.setGoal(new GoalNear(bot.blockAt(signs[i]).position.x, bot.entity.position.y, bot.entity.position.z, 0))
-
-          const chestPos = new Vec3(bot.blockAt(signs[i]).position.x, bot.blockAt(signs[i]).position.y, bot.blockAt(signs[i]).position.z+1)
-          const item = bot.inventory.slots[j]
-          bot.openBlock(bot.blockAt(chestPos)).then(function(chest) {
-            chest.deposit(item.type, null, item.count).then(function(item) {
-              bot.closeWindow(chest)
-            })
-          })
-        }
-      }
-    }
-  }
-}
-
-bot.once("spawn", () => {
-  const mcData = require("minecraft-data")(bot.version)
-  const defaultMove = new Movements(bot, mcData)
-
-  bot.on("chat", function(username, message) {
-    if (username === bot.username) return
-
-    const target = bot.players[username] ? bot.players[username].entity : null
-    if (message === 'come') {
-      if (!target) {
-        bot.chat('I don\'t see you !')
-        return
-      }
-      const p = target.position
-
-      bot.pathfinder.setMovements(defaultMove)
-      bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
-    }
-    if (message.includes("pos")) {
-      goHome()
-    }
-    if (message == "test") {
-      storeItems()
-    }
-  })
+let mcData
+bot.once('inject_allowed', () => {
+  mcData = require('minecraft-data')(bot.version)
 })
 
 bot.on("chat", (username, message) => {
-  if (message.includes("is trying to trade with you.")) {
-    isTrader(username)
+  if (username == bot.username) return
+  switch (true) {
+    case /^chest$/.test(message):
+      watchChest(["chest", "trapped_chest"])
+      break
+
+    // Temporary testing commands
+    case /^home$/.test(message):
+      goHome()
+      break
+    
+    case /^sort$/.test(message):
+      inventoryToChests()
+      break
   }
 })
 
-// Log errors and kick reasons:
-bot.on('kicked', console.log)
-bot.on('error', console.log)
+function goHome() {
+  // Temporary testing function
+  // Replacing /warp home for Hypixel
+
+  let target = bot.findBlock({
+    matching: 152,
+    maxDistance: 128
+  })
+  if (target) {
+    let target = bot.findBlock({
+      matching: 152,
+      maxDistance: 128
+    })
+    
+    moveTo(target.position)
+  } else {
+    return
+  }
+}
+
+async function inventoryToChests() {
+  // Sort away all inventory items into chests
+
+  let items = bot.inventory.items()
+  console.log(items)
+  let item = itemByName(items, "cobblestone")
+  console.log(item)
+  console.log(findChest(item))
+}
+
+function findChest(item) {
+  let chestSigns = bot.findBlocks({
+    matching: mcData.blocksByName["wall_sign"].id,
+    maxDistance: 256,
+    count: 256
+  })
+  console.log(chestSigns)
+
+  let sign
+  for (var i = 0; i < chestSigns.length; i++) {
+    sign = bot.blockAt(chestSigns[i])
+    if (sign.signText.includes(item.displayName)) {
+      return getChestSign(sign.position)
+    }
+  }
+  return null
+}
+
+async function watchChest(blocks=[]) {
+  let chestToOpen = bot.findBlock({
+    matching: blocks.map(name => mcData.blocksByName[name].id),
+    maxDistance: 6
+  })
+  const chest = await bot.openChest(chestToOpen)
+  console.log(chest.containerItems())
+
+  function closeChest() {
+    chest.close()
+  }
+
+  async function depositItem(name, amount) {
+    // Move a specified item and amount into the chest
+
+    const item = itemByName(chest.items(), name)
+    if (item) {
+      try {
+        await chest.deposit(item.type, null, amount)
+      } catch (err) {
+        ;
+      }
+    } else {
+      ;
+    }
+  }
+}
+
+async function getChestSign(signPos) {
+  // Find the chest that the sign is attached to
+
+  bot.lookAt(signPos)
+  let direction = isFacing()
+  let pos
+  if (direction == "north") {
+    pos = v(signPos.x, signPos.y, signPos.z-1)
+  } else if (direction == "east") {
+    pos = v(signPos.x, signPos.y, signPos.z+1)
+  } else if (direction == "south") {
+    pos = v(signPos.x-1, signPos.y, signPos.z)
+  } else if (direction == "west") {
+    pos = v(signPos.x-1, signPos.y, signPos.z)
+  } else {
+    pos = null
+  }
+  
+  return pos
+}
+
+async function moveTo(pos) {
+  // Moves bot to specified position
+
+  const defaultMove = new Movements(bot, mcData)
+
+  bot.pathfinder.setMovements(defaultMove)
+  bot.pathfinder.setGoal(new GoalNear(pos.x, pos.y+1, pos.z, 0))
+}
+
+function itemInSign(name) {
+  ;
+}
+
+function isFacing() {
+  // Algorithm to tell which way the bot is facing
+  // Used to determine what the position of the chest behind the sign is
+
+  let blockPos = bot.blockAtCursor().position
+  let playerPos = bot.entity.position
+
+  if (playerPos.x > blockPos.x) {
+    return "west"
+  } else if (playerPos.x < blockPos.x) {
+    return "east"
+  } else if (playerPos.z > blockPos.z) {
+    return "north"
+  } else if (playerPos.z < blockPos.z) {
+    return "south"
+  } else {
+    return null
+  }
+}
+
+function itemByName(items, name) {
+  // Get item by name from a list of items
+
+  let item
+  let i
+  for (i = 0; i < items.length; i++) {
+    item = items[i]
+    if (item && item.name == name) return item
+  }
+  return null
+}
